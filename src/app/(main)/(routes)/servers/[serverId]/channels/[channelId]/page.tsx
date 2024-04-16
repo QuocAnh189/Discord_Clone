996957d808b6b2d9d5536153dbeabcb77860ddd5
QuocAnh189 <anhquoc18092003@gmail.com>
2024-04-16 19:01:10 +0700

    update

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,7 @@
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-// import { ChannelType } from "@prisma/client";
+import { ChannelType } from "@prisma/client";
 
-//components
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
@@ -50,7 +49,7 @@ const ChannelIdPage = async ({
         serverId={channel.serverId}
         type="channel"
       />
-      {channel.type === 'TEXT' && (
+      {channel.type === ChannelType.TEXT && (
         <>
           <ChatMessages
             member={member}
@@ -77,14 +76,14 @@ const ChannelIdPage = async ({
           />
         </>
       )}
-      {channel.type === 'AUDIO' && (
+      {channel.type === ChannelType.AUDIO && (
         <MediaRoom
           chatId={channel.id}
           video={false}
           audio={true}
         />
       )}
-      {channel.type === 'VIDEO' && (
+      {channel.type === ChannelType.VIDEO && (
         <MediaRoom
           chatId={channel.id}
           video={true}
